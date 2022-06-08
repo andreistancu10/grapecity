244e78c7c56d94d814b1157c36b0d070e0629b96
Ciprian Rosca <ciprian.rosca@htss.ro>
2022-06-08 17:34:26 +0300

    removed registrationNumber prop from request object as it will be populated o the backend

--- DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Business/IncomingDocuments/Commands/Create/CreateIncomingDocumentCommand.cs
+++ DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Business/IncomingDocuments/Commands/Create/CreateIncomingDocumentCommand.cs
@@ -7,7 +7,6 @@ namespace DigitNow.Domain.DocumentManagement.Business.IncomingDocuments.Commands
     public class CreateIncomingDocumentCommand : ICommand<ResultObject>
     {
         public DateTime? RegistrationDate { get; set; }
-        public int RegistrationNumber { get; set; }
         public int InputChannelId { get; set; }
         public int IssuerTypeId { get; set; }
         public string IssuerName { get; set; }
