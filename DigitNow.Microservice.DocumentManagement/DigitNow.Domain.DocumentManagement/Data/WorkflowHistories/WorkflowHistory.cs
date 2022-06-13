c7bfaa73f3cf47bc817e9337b2f67cc6cd05b99d
Ciprian Rosca <ciprian.rosca@htss.ro>
2022-06-13 14:57:33 +0300

    create enums for recipienttype and status

--- DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Data/WorkflowHistories/WorkflowHistory.cs
+++ DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Data/WorkflowHistories/WorkflowHistory.cs
@@ -5,13 +5,11 @@ namespace DigitNow.Domain.DocumentManagement.Data.WorkflowHistories
     public class WorkflowHistory
     {
         public int Id { get; set; }
-        public int InitiatorType { get; set; }
-        public string InitiatorName { get; set; }
-        public int ActionType { get; set; }
+        public int RecipientType { get; set; }
+        public string RecipientId { get; set; }
+        public int? ActionType { get; set; }
         public string Remarks { get; set; }
         public string DeclineReason { get; set; }
-        public string DestinationType { get; set; }
-        public string DestinationName { get; set; }
         public string Resolution { get; set; }
         public DateTime? OpionionRequestedUntil { get; set; }
         public int Status { get; set; }
