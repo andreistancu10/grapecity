f319917fd4a3fd5bd46fbf078abd79220ca9cab0
Ciprian Rosca <ciprian.rosca@htss.ro>
2022-06-16 11:11:29 +0300

    adding the AuthorizationToken when calling another microservice

--- DigitNow.Microservice.DocumentManagement/DigitNow.Adapters.MS.Identity/IdentityHttpClient.cs
+++ DigitNow.Microservice.DocumentManagement/DigitNow.Adapters.MS.Identity/IdentityHttpClient.cs
@@ -1,4 +1,8 @@
 ﻿using Newtonsoft.Json;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication;
+using System.Net.Http.Headers;
 
 namespace DigitNow.Adapters.MS.Identity
 {
@@ -16,6 +20,8 @@ namespace DigitNow.Adapters.MS.Identity
 
         public async Task<T> GetAsync<T>(string requestUri) where T : class
         {
+            await SetAuthorizationTokenAsync();
+
             var response = await _httpClient.GetAsync(requestUri);
             if (!response.IsSuccessStatusCode)
             {
@@ -31,6 +37,13 @@ namespace DigitNow.Adapters.MS.Identity
             }
 
             return result;
+        }
+
+        private async Task SetAuthorizationTokenAsync()
+        {
+            var httpContextAccessor = _serviceProvider.GetService<IHttpContextAccessor>();
+            var token = await httpContextAccessor.HttpContext.GetTokenAsync("access_token");
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
         }
     }
 }
