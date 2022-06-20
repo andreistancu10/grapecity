b57a4b2e2440453f07e555bbc3f29d4c31d0c94c
Gabriel Ciuca Albu <albu.gabriel.oc@gmail.com>
2022-06-20 13:04:32 +0300

    Start file upload service

---
+++ DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Business/_Common/Documents/Services/FileUploadService.cs
@@ -0,0 +1,15 @@
+﻿namespace DigitNow.Domain.DocumentManagement.Business._Common.Documents.Services;
+
+public class FileUploadService
+{
+    private readonly int _depth;
+    private readonly int _filesPerDirectory;
+    private readonly string _filename;
+
+    public FileUploadService(int depth, int filesPerDirectory, string filename)
+    {
+        _depth = depth;
+        _filesPerDirectory = filesPerDirectory;
+        _filename = filename;
+    }
+}
