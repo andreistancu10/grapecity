e7716c52327475e6f13054fc2bcffa052aaab107
Gabriel Ciuca Albu <albu.gabriel.oc@gmail.com>
2022-06-27 22:09:36 +0300

    Fix use msdb;

--- DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Migrations/20220627182443_SpecialRegisterAssociations_IncrementIdStoredProcedure.cs
+++ DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Migrations/20220627182443_SpecialRegisterAssociations_IncrementIdStoredProcedure.cs
@@ -24,6 +24,7 @@ namespace DigitNow.Domain.DocumentManagement.Migrations
 					GO";
 
 			var job = @"
+				use msdb;
 				EXEC dbo.sp_add_job  
 					@job_name = N'Set Association Ids' ;  
 				GO  
@@ -35,16 +36,17 @@ namespace DigitNow.Domain.DocumentManagement.Migrations
 					@retry_attempts = 5,  
 					@retry_interval = 5 ;  
 				GO  
+				use msdb;
 				EXEC dbo.sp_add_schedule  
 					@schedule_name = N'RunOnce',  
 					@freq_type = 4,  
-					@freq_subday_type = "+"0x1"+@",   
-				    @freq_subday_interval = 20,  
-				    @active_start_time = 233000;
+					@freq_subday_type = " + "0x1"+@",   
+					@freq_subday_interval = 20,  
+					@active_start_time = 233000;
 				GO
 					EXEC sp_attach_schedule
 					@job_name = N'Set Association Ids',  
-				    @schedule_name = N'RunEvery15s';
+					@schedule_name = N'RunEvery15s';
 				GO
 					EXEC dbo.sp_add_jobserver
 					@job_name = N'Set Association Ids';
