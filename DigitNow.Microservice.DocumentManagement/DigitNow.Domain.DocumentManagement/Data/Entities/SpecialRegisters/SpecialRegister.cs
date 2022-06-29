38a92b2d9b8945089f4dc1912d5c1d0010b1d477
Gabriel Ciuca Albu <albu.gabriel.oc@gmail.com>
2022-06-29 13:47:15 +0300

    Fix SpecialRegister entity

--- DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Data/Entities/SpecialRegisters/SpecialRegister.cs
+++ DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Data/Entities/SpecialRegisters/SpecialRegister.cs
@@ -2,15 +2,9 @@
 
 namespace DigitNow.Domain.DocumentManagement.Data.Entities.SpecialRegisters;
 
-public class SpecialRegister : IExtendedEntity
+public class SpecialRegister : ExtendedEntity
 {
-    public long Id { get; set; }
     public string Name { get; set; }
     public string Observations { get; set; }
     public int DocumentType { get; set; }
-
-    public DateTime CreatedAt { get; set; }
-    public long CreatedBy { get; set; }
-    public DateTime ModifiedAt { get; set; }
-    public long ModifiedBy { get; set; }
 }
