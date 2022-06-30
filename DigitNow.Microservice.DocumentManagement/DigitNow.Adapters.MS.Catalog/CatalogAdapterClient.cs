81310cb896e0070f0d6612511f3877d3e123c64d
thiraiulia <iulia.thira@rebeldot.com>
2022-06-30 14:08:16 +0300

    feat: created catalog adapter

---
+++ DigitNow.Microservice.DocumentManagement/DigitNow.Adapters.MS.Catalog/CatalogAdapterClient.cs
@@ -0,0 +1,20 @@
+﻿using DigitNow.Adapters.MS.Catalog.Poco;
+
+namespace DigitNow.Adapters.MS.Catalog
+{
+    public interface ICatalogAdapterClient
+    {
+        Task<DocumentType> GetDocumentTypeById(int id, CancellationToken cancellationToken);
+    }
+    public class CatalogAdapterClient : ICatalogAdapterClient
+    {
+        private readonly CatalogHttpClient _catalogHttpClient;
+
+        public CatalogAdapterClient(CatalogHttpClient catalogHttpClient)
+        {
+            _catalogHttpClient = catalogHttpClient;
+        }
+        public Task<DocumentType> GetDocumentTypeById(int id, CancellationToken cancellationToken) => _catalogHttpClient.GetAsync<DocumentType>($"api/DocumentTypes/{id}", cancellationToken);
+        
+    }
+}
