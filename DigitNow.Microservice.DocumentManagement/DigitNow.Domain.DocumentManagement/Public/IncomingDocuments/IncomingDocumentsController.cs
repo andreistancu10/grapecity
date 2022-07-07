93e6a5a36b0b084f56cbcd0209d792f85413d3d3
thiraiulia <iulia.thira@rebeldot.com>
2022-07-07 10:01:50 +0300

    refactor

--- DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Public/IncomingDocuments/IncomingDocumentsController.cs
+++ DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Public/IncomingDocuments/IncomingDocumentsController.cs
@@ -14,8 +14,7 @@ using System.Threading.Tasks;
 
 namespace DigitNow.Domain.DocumentManagement.Public.IncomingDocuments;
 
-//[Authorize]
-[AllowAnonymous]
+[Authorize]
 [ApiController]
 [Route("api/incoming-documents")]
 public class IncomingDocumentsController : ApiController
