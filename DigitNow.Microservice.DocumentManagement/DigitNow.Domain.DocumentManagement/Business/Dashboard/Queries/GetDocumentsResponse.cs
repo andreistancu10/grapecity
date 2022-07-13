25c0834cc358b827335460948319b3385f999f9c
Bogdan Botea <bogdan.botea@htss.ro>
2022-07-13 16:55:02 +0300

    Code review followup

--- DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Business/Dashboard/Queries/GetDocumentsResponse.cs
+++ DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Business/Dashboard/Queries/GetDocumentsResponse.cs
@@ -1,14 +1,15 @@
 ﻿using DigitNow.Domain.DocumentManagement.Business.Common.Models;
 using System.Collections.Generic;
 
-namespace DigitNow.Domain.DocumentManagement.Business.Dashboard.Queries;
-
-public class GetDocumentsResponse
+namespace DigitNow.Domain.DocumentManagement.Business.Dashboard.Queries
 {
-    public long TotalItems { get; set; }
-    public int PageNumber { get; set; }
-    public int PageSize { get; set; }
-    public long TotalPages { get; set; }
+    public class GetDocumentsResponse
+    {
+        public long TotalItems { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public long TotalPages { get; set; }
 
-    public IList<DocumentViewModel> Documents { get; set; }
+        public IList<DocumentViewModel> Documents { get; set; }
+    }
 }
