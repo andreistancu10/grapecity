c102ec70c6de1dc6d820d888199a608f4ffc25ee
Bogdan Botea <bogdan.botea@htss.ro>
2022-07-21 10:11:55 +0300

    Changed fetchers from RPC to HTTP

--- DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Business/Common/ModelsFetchers/ConcreteFetchers/DocumentsCategoriesFetcher.cs
+++ DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Business/Common/ModelsFetchers/ConcreteFetchers/DocumentsCategoriesFetcher.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
+using DigitNow.Adapters.MS.Catalog;
 using DigitNow.Domain.Catalog.Client;
 using DigitNow.Domain.DocumentManagement.Business.Common.Models;
 using DigitNow.Domain.DocumentManagement.Business.Common.ModelsFetchers.ConcreteFetchersContexts;
@@ -15,14 +16,28 @@ namespace DigitNow.Domain.DocumentManagement.Business.Common.ModelsFetchers.Conc
     {
         private readonly ICatalogClient _catalogClient;
         private readonly IMapper _mapper;
+        private readonly ICatalogAdapterClient _catalogAdapterClient;
 
         public DocumentsCategoriesFetcher(IServiceProvider serviceProvider)
         {
             _catalogClient = serviceProvider.GetService<ICatalogClient>();
             _mapper = serviceProvider.GetService<IMapper>();
+            _catalogAdapterClient = serviceProvider.GetService<ICatalogAdapterClient>();
         }
 
         protected override async Task<List<DocumentCategoryModel>> FetchInternalAsync(DocumentsFetcherContext context, CancellationToken cancellationToken)
+        {
+            var documentTypes = await _catalogAdapterClient.GetDocumentTypesAsync(cancellationToken);
+
+            var documentCategoryModels = documentTypes
+                .Select(x => _mapper.Map<DocumentCategoryModel>(x))
+                .ToList();
+
+            return documentCategoryModels;
+        }
+
+        [Obsolete("This will be investigated in the future")]
+        private async Task<List<DocumentCategoryModel>> FetchInternalAsync_Rpc(DocumentsFetcherContext context, CancellationToken cancellationToken)
         {
             var documentTypesResponse = await _catalogClient.DocumentTypes.GetDocumentTypesAsync(cancellationToken);
 
--- DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Business/Common/ModelsFetchers/ConcreteFetchers/DocumentsInternalCategoriesFetcher.cs
+++ DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Business/Common/ModelsFetchers/ConcreteFetchers/DocumentsInternalCategoriesFetcher.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
+using DigitNow.Adapters.MS.Catalog;
 using DigitNow.Domain.Catalog.Client;
 using DigitNow.Domain.DocumentManagement.Business.Common.Models;
 using DigitNow.Domain.DocumentManagement.Business.Common.ModelsFetchers.ConcreteFetchersContexts;
@@ -15,18 +16,29 @@ namespace DigitNow.Domain.DocumentManagement.Business.Common.ModelsFetchers.Conc
     {
         private readonly ICatalogClient _catalogClient;
         private readonly IMapper _mapper;
+        private readonly ICatalogAdapterClient _catalogAdapterClient;
 
         public DocumentsInternalCategoriesFetcher(IServiceProvider serviceProvider)
         {
             _catalogClient = serviceProvider.GetService<ICatalogClient>();
             _mapper = serviceProvider.GetService<IMapper>();
+            _catalogAdapterClient = serviceProvider.GetService<ICatalogAdapterClient>();
         }
 
         protected override async Task<List<DocumentCategoryModel>> FetchInternalAsync(DocumentsFetcherContext context, CancellationToken cancellationToken)
         {
-            //return Task.FromResult(new List<DocumentCategoryModel>());
+            var internalDocumentTypes = await _catalogAdapterClient.GetInternalDocumentTypesAsync(cancellationToken);
 
-			//TODO: Investigate further the RabbitMQ issue
+            var internalDocumentCategoryModels = internalDocumentTypes
+                .Select(x => _mapper.Map<DocumentCategoryModel>(x))
+                .ToList();
+
+            return internalDocumentCategoryModels;
+        }
+
+        [Obsolete("This will be investigated in the future")]
+        private async Task<List<DocumentCategoryModel>> FetchInternalAsync_Rpc(DocumentsFetcherContext context, CancellationToken cancellationToken)
+        {
             var internalDocumentTypesResponse = await _catalogClient.InternalDocumentTypes.GetInternalDocumentTypesAsync(cancellationToken);
 
             // Note: DocumentTypes is actual DocumentCategory
