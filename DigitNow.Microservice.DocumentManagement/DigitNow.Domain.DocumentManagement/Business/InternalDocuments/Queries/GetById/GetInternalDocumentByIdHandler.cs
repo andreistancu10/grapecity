e0aa8525c543858582b3843986cecca2383f8888
Ciprian Rosca <ciprian.rosca@htss.ro>
2022-07-21 15:07:59 +0300

    remove relationship to UploadedFiles

--- DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Business/InternalDocuments/Queries/GetById/GetInternalDocumentByIdHandler.cs
+++ DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Business/InternalDocuments/Queries/GetById/GetInternalDocumentByIdHandler.cs
@@ -22,8 +22,6 @@ namespace DigitNow.Domain.DocumentManagement.Business.InternalDocuments.Queries.
         {
             var foundInternalDocument = await _dbContext.InternalDocuments
                 .Include(x => x.WorkflowHistory)
-                .Include(x => x.Document)
-                .ThenInclude(x => x.DocumentUploadedFiles)
                 .FirstOrDefaultAsync(c => c.DocumentId == request.Id, cancellationToken);
             
             if (foundInternalDocument == null) return null;
