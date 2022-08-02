aeaafd35f6d245ad252a19a11de98169f59a5ccf
Cristian Neagu <Cristian.Neagu@rinf.tech>
2022-08-02 18:18:51 +0300

    remove month interval

--- DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Business/OperationalArchive/Commands/Update/MoveDocumentsToArchiveHandler.cs
+++ DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Business/OperationalArchive/Commands/Update/MoveDocumentsToArchiveHandler.cs
@@ -2,26 +2,24 @@
 using DigitNow.Domain.DocumentManagement.Data;
 using HTSS.Platform.Core.CQRS;
 using Microsoft.EntityFrameworkCore;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace DigitNow.Domain.DocumentManagement.Business.OperationalArchive.Commands.Update
 {
     public class MoveDocumentsToArchiveHandler : ICommandHandler<MoveDocumentsToArchiveCommand, ResultObject>
     {
         private readonly DocumentManagementDbContext _dbContext;
-        private readonly int _archivingPeriod = 6;
+        //TODO: revert back to month interval
+        //private readonly int _archivingPeriod = 6;
 
         public MoveDocumentsToArchiveHandler(DocumentManagementDbContext dbContext)
         {
             _dbContext = dbContext;
         }
         public async Task<ResultObject> Handle(MoveDocumentsToArchiveCommand request, CancellationToken cancellationToken)
-        {          
-            var documents = await  _dbContext.Documents
+        {
+            var documents = await _dbContext.Documents
                 .Where(x => !x.IsArchived)
-                .Where(x => x.StatusModifiedAt.Date.AddMonths(_archivingPeriod) < System.DateTime.Now.Date)
+                //.Where(x => x.StatusModifiedAt.Date.AddMonths(_archivingPeriod) < System.DateTime.Now.Date)
                 .Where(x => x.Status == DocumentStatus.Finalized || (x.DocumentType == DocumentType.Internal && x.Status == DocumentStatus.InWorkCountersignature))
                 .ToListAsync(cancellationToken);
 
