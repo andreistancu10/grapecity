9d5138bceb14198a7dd52a7dcd94271e37ee91a9
Gabriel Ciuca Albu <albu.gabriel.oc@gmail.com>
2022-08-03 18:39:07 +0300

    Refactor

--- DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement.Contracts/Documents/Enums/DocumentTypeRo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DigitNow.Domain.DocumentManagement.Contracts.Documents.Enums
-{
-    public enum DocumentTypeRo
-    {
-        Intrare = DocumentType.Incoming,
-        Intern = DocumentType.Internal,
-        Iesire = DocumentType.Outgoing
-    }
-}
---
+++ DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/.utils/DocumentTypeTranslation.cs
@@ -0,0 +1,16 @@
+﻿using DigitNow.Domain.DocumentManagement.Contracts.Documents.Enums;
+
+namespace DigitNow.Domain.DocumentManagement.utils
+{
+    public class DocumentTypeTranslation
+    {
+        public DocumentType DocumentType { get; set; }
+        public string DocumentTypeLabel { get; set; }
+
+        public DocumentTypeTranslation(DocumentType documentType, string documentTypeLabel)
+        {
+            DocumentType = documentType;
+            DocumentTypeLabel = documentTypeLabel;
+        }
+    }
+}
