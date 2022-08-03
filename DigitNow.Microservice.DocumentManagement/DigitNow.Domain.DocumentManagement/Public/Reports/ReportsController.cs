e47caf0e93c6624697e4501b5ab5b65179cd6b2a
Gabriel Ciuca Albu <albu.gabriel.oc@gmail.com>
2022-08-03 18:58:02 +0300

    Fix todo

--- DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Public/Reports/ReportsController.cs
+++ DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Public/Reports/ReportsController.cs
@@ -3,8 +3,8 @@ using System.Threading.Tasks;
 using AutoMapper;
 using DigitNow.Domain.DocumentManagement.Business.Common.ViewModels;
 using DigitNow.Domain.DocumentManagement.Business.Reports.Queries;
-using DigitNow.Domain.DocumentManagement.Contracts.Documents.Enums;
 using DigitNow.Domain.DocumentManagement.Public.Reports.Models;
+using Domain.Localization.Client;
 using HTSS.Platform.Core.Files;
 using HTSS.Platform.Infrastructure.Api.Tools;
 using MediatR;
@@ -21,48 +21,50 @@ namespace DigitNow.Domain.DocumentManagement.Public.Reports
         private readonly IMediator _mediator;
         private readonly IMapper _mapper;
         private readonly IExportService<ExportReportViewModel> _exportService;
+        private readonly ILocalizationManager _localizationManger;
 
         public ReportsController(
             IMediator mediator,
             IMapper mapper,
-            IExportService<ExportReportViewModel> exportService)
+            IExportService<ExportReportViewModel> exportService,
+            ILocalizationManager localizationManger)
         {
             _mediator = mediator;
             _mapper = mapper;
             _exportService = exportService;
+            _localizationManger = localizationManger;
         }
 
         [HttpPost("expired")]
-        public async Task<IActionResult> GetReportAsync([FromBody] GetExpiredReportRequest request)
+        public async Task<IActionResult> GetReportAsync([FromBody] GetExpiredReportRequest request, CancellationToken cancellationToken)
         {
             var getReportQuery = _mapper.Map<GetReportQuery>(request);
-            var reportResult = await _mediator.Send(getReportQuery);
+            var reportResult = await _mediator.Send(getReportQuery, cancellationToken);
 
             if (reportResult == null)
             {
                 return NotFound();
             }
 
-            //TODO: Use translations for file name
-            var fileResult = await _exportService.CreateExcelFile("Documente expirate", "DocumentsSheet", reportResult);
+            var documentNameResponse = await _localizationManger.Translate(1, "dms.reports.expired-document-name", cancellationToken);
+            var fileResult = await _exportService.CreateExcelFile(documentNameResponse.Translation, "DocumentsSheet", reportResult);
 
             return File(fileResult.Content, fileResult.ContentType, fileResult.Name);
         }
 
-
         [HttpPost("to-expire")]
-        public async Task<IActionResult> GetReportAsync([FromBody] GetToExpireReportRequest request)
+        public async Task<IActionResult> GetReportAsync([FromBody] GetToExpireReportRequest request, CancellationToken cancellationToken)
         {
             var getReportQuery = _mapper.Map<GetReportQuery>(request);
-            var reportResult = await _mediator.Send(getReportQuery);
+            var reportResult = await _mediator.Send(getReportQuery, cancellationToken);
 
             if (reportResult == null)
             {
                 return NotFound();
             }
 
-            //TODO: Use translations for file name
-            var fileResult = await _exportService.CreateExcelFile("Documente ce urmeaza sa expire", "DocumentsSheet", reportResult);
+            var documentNameResponse = await _localizationManger.Translate(1, "dms.reports.to-expire-document-name", cancellationToken);
+            var fileResult = await _exportService.CreateExcelFile(documentNameResponse.Translation, "DocumentsSheet", reportResult);
 
             return File(fileResult.Content, fileResult.ContentType, fileResult.Name);
         }
