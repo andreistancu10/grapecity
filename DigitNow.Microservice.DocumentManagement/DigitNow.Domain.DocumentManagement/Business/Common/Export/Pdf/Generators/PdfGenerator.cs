e99896043094344ec56afb3460fe8594de8dce77
thiraiulia <iulia.thira@rebeldot.com>
2022-08-19 14:02:51 +0300

    Update PdfGenerator.cs

--- DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Business/Common/Export/Pdf/Generators/PdfGenerator.cs
+++ DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Business/Common/Export/Pdf/Generators/PdfGenerator.cs
@@ -53,7 +53,7 @@ namespace DigitNow.Domain.DocumentManagement.Business.Common.Export.Pdf.Generato
         private static string GetTemplateContent(string templateName)
         {
             var templateResourceName = _currentAssemblyResourceNames.FirstOrDefault(x => x.EndsWith(templateName));
-            if (templateResourceName == null) throw new ArgumentNullException("No template resource name");
+            if (templateResourceName == null) throw new ArgumentException("Template resource name cannot be null", "GetTemplateContent");
 
             var stream = _currentAssembly.GetManifestResourceStream(templateResourceName);
 
