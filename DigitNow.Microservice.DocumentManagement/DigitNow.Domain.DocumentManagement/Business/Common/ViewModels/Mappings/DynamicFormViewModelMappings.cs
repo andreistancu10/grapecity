aa7224a5830e73e95bf371f20f4b0438ceb86f3f
Gabriel Ciuca Albu <albu.gabriel.oc@gmail.com>
2022-08-29 17:41:37 +0300

    Code review followup, testing and migration

--- DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Business/Common/ViewModels/Mappings/DynamicFormViewModelMappings.cs
+++ DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Business/Common/ViewModels/Mappings/DynamicFormViewModelMappings.cs
@@ -8,12 +8,11 @@ namespace DigitNow.Domain.DocumentManagement.Business.Common.ViewModels.Mappings
         public DynamicFormViewModelMappings()
         {
             CreateMap<DynamicForm, DynamicFormViewModel>()
-                .ForMember(dest => dest.DynamicFormDetails.Context, opt => opt.MapFrom(src => src.Context))
-                .ForMember(dest => dest.DynamicFormDetails.Description, opt => opt.MapFrom(src => src.Description))
-                .ForMember(dest => dest.DynamicFormDetails.Id, opt => opt.MapFrom(src => src.Id))
-                .ForMember(dest => dest.DynamicFormDetails.Label, opt => opt.MapFrom(src => src.Label))
-                .ForMember(dest => dest.DynamicFormDetails.Name, opt => opt.MapFrom(src => src.Name))
-                .ForMember(dest => dest.DynamicFormControls, opt => opt.MapFrom(src => src.Name));
+                .ForPath(dest => dest.DynamicFormDetails.Context, opt => opt.MapFrom(src => src.Context))
+                .ForPath(dest => dest.DynamicFormDetails.Description, opt => opt.MapFrom(src => src.Description))
+                .ForPath(dest => dest.DynamicFormDetails.Id, opt => opt.MapFrom(src => src.Id))
+                .ForPath(dest => dest.DynamicFormDetails.Label, opt => opt.MapFrom(src => src.Label))
+                .ForPath(dest => dest.DynamicFormDetails.Name, opt => opt.MapFrom(src => src.Name));
         }
     }
 }
