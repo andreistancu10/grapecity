9017b0d8247c33da37498204d16d7ea22f2ecf5c
Bogdan Botea <bogdan.botea@htss.ro>
2022-08-30 17:57:30 +0300

    Code review followup

--- DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Business/Common/ViewModels/Mappings/ExcelReportViewModelMappings.cs
+++ DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Business/Common/ViewModels/Mappings/ExcelReportViewModelMappings.cs
@@ -9,17 +9,12 @@ namespace DigitNow.Domain.DocumentManagement.Business.Common.ViewModels.Mappings
         public ExcelReportViewModelMappings()
         {
             CreateMap<ReportViewModel, ExportReportViewModel>()
-               .ForMember(c => c.RegistrationDate, opt => opt.MapFrom(src => src.RegistrationDate))
-               .ForMember(c => c.RegistrationNumber, opt => opt.MapFrom(src => src.RegistrationNumber))
                .ForMember(c => c.Recipient, opt => opt.MapFrom(src => src.Recipient.Name))
                .ForMember(c => c.Issuer, opt => opt.MapFrom(src => src.Issuer.Name))
                .ForMember(c => c.CurrentStatus, opt => opt.MapFrom(src => src.Issuer.Name))
                .ForMember(c => c.DocumentType, opt => opt.MapFrom(src => src.DocumentType.Label))
                .ForMember(c => c.DocumentCategory, opt => opt.MapFrom(src => src.DocumentCategory.Name))
                .ForMember(c => c.Functionary, opt => opt.MapFrom<MapFunctionary>())
-               .ForMember(c => c.AllocationDate, opt => opt.MapFrom(src => src.AllocationDate))
-               .ForMember(c => c.ResolutionDate, opt => opt.MapFrom(src => src.ResolutionDate))
-               .ForMember(c => c.Expired, opt => opt.MapFrom(src => src.Expired))
                .ForMember(c => c.SpecialRegister, opt => opt.MapFrom(src => src.SpecialRegister.Name));
         }
 
