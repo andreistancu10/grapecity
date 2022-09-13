21e9a1e0621c9fc38d6ead1e756f520f9066ef10
Gabriel Ciuca Albu <gabriel.albu@htss.ro>
2022-09-13 14:20:03 +0300

    Get activity information on get action by id

--- DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Business/Actions/Queries/GetById/GetActionByIdHandler.cs
+++ DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Business/Actions/Queries/GetById/GetActionByIdHandler.cs
@@ -1,5 +1,10 @@
 ﻿using AutoMapper;
+using DigitNow.Domain.DocumentManagement.Business.Common.ModelsAggregates;
+using DigitNow.Domain.DocumentManagement.Business.Common.ModelsFetchers.ConcreteFetchersContexts;
+using DigitNow.Domain.DocumentManagement.Business.Common.ModelsFetchers.Registries;
 using DigitNow.Domain.DocumentManagement.Business.Common.Services;
+using DigitNow.Domain.DocumentManagement.Business.Common.ViewModels;
+using DigitNow.Domain.DocumentManagement.Data.Entities;
 using HTSS.Platform.Core.CQRS;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,24 +14,57 @@ namespace DigitNow.Domain.DocumentManagement.Business.Actions.Queries.GetById
     {
         private readonly IMapper _mapper;
         private readonly IActionService _actionService;
+        private readonly ActivityRelationsFetcher _activityRelationsFetcher;
 
-        public GetActionByIdHandler(IMapper mapper,
+        public GetActionByIdHandler(
+            IMapper mapper,
+            IServiceProvider serviceProvider,
             IActionService actionService)
         {
             _mapper = mapper;
             _actionService = actionService;
+            _activityRelationsFetcher = new ActivityRelationsFetcher(serviceProvider);
         }
 
         public async Task<GetActionByIdResponse> Handle(GetActionByIdQuery request, CancellationToken cancellationToken)
         {
             var action = await _actionService.FindQuery()
+                .Include(c => c.AssociatedActivity)
+                .AsNoTracking()
                 .Include(x => x.ActionFunctionaries)
-                .FirstOrDefaultAsync(item => item.Id == request.Id);
+                .FirstOrDefaultAsync(item => item.Id == request.Id, cancellationToken);
 
             if (action == null)
+            {
                 return null;
+            }
 
-            return _mapper.Map<GetActionByIdResponse>(action);
+            var associatedActivity = action.AssociatedActivity;
+
+            await _activityRelationsFetcher.UseActivityFetcherContext(
+                    new ActivitiesFetcherContext
+                    {
+                        Activities = new List<Activity>
+                        {
+                            associatedActivity
+                        }
+                    })
+                .TriggerFetchersAsync(cancellationToken);
+
+            var activityViewModel =
+                _mapper.Map<ActivityAggregate, ActivityViewModel>(new ActivityAggregate
+                {
+                    Activity = associatedActivity,
+                    Departments = _activityRelationsFetcher.Departments,
+                    Users = _activityRelationsFetcher.Users,
+                    GeneralObjectives = _activityRelationsFetcher.GeneralObjective,
+                    SpecificObjectives = _activityRelationsFetcher.SpecificObjective
+                });
+
+            var response = _mapper.Map<GetActionByIdResponse>(action);
+            response.Activity = activityViewModel;
+
+            return response;
         }
     }
 }
--- DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Business/Actions/Queries/GetById/GetActionByIdResponse.cs
+++ DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Business/Actions/Queries/GetById/GetActionByIdResponse.cs
@@ -1,11 +1,12 @@
-﻿using DigitNow.Domain.DocumentManagement.Contracts.Objectives;
+﻿using DigitNow.Domain.DocumentManagement.Business.Common.ViewModels;
+using DigitNow.Domain.DocumentManagement.Contracts.Objectives;
 
 namespace DigitNow.Domain.DocumentManagement.Business.Actions.Queries.GetById
 {
     public class GetActionByIdResponse
     {
         public long Id { get; set; }
-        public long ActivityId { get; set; }
+        public ActivityViewModel Activity { get; set; }
         public long DepartmentId { get; set; }
         public string Code { get; set; }
         public string Title { get; set; }
