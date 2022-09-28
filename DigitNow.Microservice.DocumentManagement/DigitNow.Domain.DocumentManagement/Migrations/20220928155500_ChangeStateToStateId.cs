5a8239a38493072d6c6a954c7f58badfd6a2e810
Gabriel Ciuca Albu <albu.gabriel.oc@gmail.com>
2022-09-28 18:55:27 +0300

    Add migration

---
+++ DigitNow.Microservice.DocumentManagement/DigitNow.Domain.DocumentManagement/Migrations/20220928155500_ChangeStateToStateId.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DigitNow.Domain.DocumentManagement.Migrations
+{
+    public partial class ChangeStateToStateId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "State",
+                schema: "DocumentManagement",
+                table: "Risk");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                schema: "DocumentManagement",
+                table: "Objective");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                schema: "DocumentManagement",
+                table: "Activity");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                schema: "DocumentManagement",
+                table: "Action");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Deadline",
+                schema: "DocumentManagement",
+                table: "RiskControlAction",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ControlMeasurement",
+                schema: "DocumentManagement",
+                table: "RiskControlAction",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "StateId",
+                schema: "DocumentManagement",
+                table: "Risk",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "StateId",
+                schema: "DocumentManagement",
+                table: "Objective",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "StateId",
+                schema: "DocumentManagement",
+                table: "Activity",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "StateId",
+                schema: "DocumentManagement",
+                table: "Action",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StateId",
+                schema: "DocumentManagement",
+                table: "Risk");
+
+            migrationBuilder.DropColumn(
+                name: "StateId",
+                schema: "DocumentManagement",
+                table: "Objective");
+
+            migrationBuilder.DropColumn(
+                name: "StateId",
+                schema: "DocumentManagement",
+                table: "Activity");
+
+            migrationBuilder.DropColumn(
+                name: "StateId",
+                schema: "DocumentManagement",
+                table: "Action");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Deadline",
+                schema: "DocumentManagement",
+                table: "RiskControlAction",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ControlMeasurement",
+                schema: "DocumentManagement",
+                table: "RiskControlAction",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "State",
+                schema: "DocumentManagement",
+                table: "Risk",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "State",
+                schema: "DocumentManagement",
+                table: "Objective",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "State",
+                schema: "DocumentManagement",
+                table: "Activity",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "State",
+                schema: "DocumentManagement",
+                table: "Action",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
